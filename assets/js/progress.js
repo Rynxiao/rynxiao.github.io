b2414e93b9bd10422cdb069b49a7d46123974f97
rynxiao <yuzhongzi91@sina.com>
2017-09-04 17:53:03 +0800

    modify progress script

--- assets/js/progress.js
+++ assets/js/progress.js
@@ -24,20 +24,21 @@
 	var ele = document.querySelector('#rynProgress');
 	var windowHeight = getWindowHeight();
 	var scrollHeight = getScrollHeight();
+	var trueHeight = scrollHeight - windowHeight;
 	var top = getScrollTop();
 
 	if (scrollHeight <= windowHeight) {
 		ele.style.display = 'none';
 	} else {
-		ele.setAttribute('max', scrollHeight);
+		ele.setAttribute('max', trueHeight);
 
 		// 添加事件
 		document.addEventListener('scroll', function() {
 			var top = getScrollTop();
-			var currentHeight = top + windowHeight;
-			var percent = Number(currentHeight / scrollHeight) + '%';
-			ele.setAttribute('value', currentHeight);
-			ele.setAttribute('alt', percent);
+			var percent = Number(top / trueHeight).toFixed(2) + '%';
+			ele.setAttribute('value', top);
+			ele.setAttribute('title', percent);
+			ele.textContent = percent;
 		});
 	}
 })();
